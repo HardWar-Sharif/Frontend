cc3a9f317ad73c16b7d85171e214b1faaec54f3c
Amirhossein Souri <amirhossein.souri@hamravesh.com>
2025-01-10 03:29:55 +0330

    setup chakra-ui with dark mode and red theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Landing from "./pages/Landing";
 
-createRoot(document.getElementById('root')!).render(
+import { ColorModeProvider } from "@/components/ui/color-mode";
+import { system } from "./theme";
+import { ChakraProvider } from "@chakra-ui/react";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ChakraProvider value={system}>
+      <ColorModeProvider>
+        <Landing />
+      </ColorModeProvider>
+    </ChakraProvider>
+  </StrictMode>
+);
---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,15 @@
+import { Box, Button, Input } from "@chakra-ui/react";
+import React from "react";
+
+const Landing = () => {
+  return (
+    <Box>
+      <section>
+        <Input />
+      </section>
+      <Button>Hello</Button>
+    </Box>
+  );
+};
+
+export default Landing;
