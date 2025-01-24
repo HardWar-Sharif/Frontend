853bd9b9ce549b52a67ecd9a3e496149be78cc35
Aref Zarezadeh <arefzarezade@gmail.com>
2025-01-24 23:37:01 +0330

    added navbar

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ const tolgee = Tolgee()
   .use(DevTools())
   .use(FormatSimple())
   .init({
-    language: "fa",
+    language: "en",
 
     // for development
     apiUrl: import.meta.env.VITE_APP_TOLGEE_API_URL,
@@ -27,12 +27,12 @@ const tolgee = Tolgee()
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <LocaleProvider locale="fa">
+    <LocaleProvider locale="en">
       <TolgeeProvider tolgee={tolgee}>
         <ChakraProvider value={system}>
           <Fonts />
           <ColorModeProvider>
-            <div dir="rtl">
+            <div dir="ltr">
               <Landing />
             </div>
           </ColorModeProvider>
