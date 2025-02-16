5e802d9eda42cfc20fd069419a28b5536d8ee566
Amirhossein Souri <amirhsnsouri@gmail.com>
2025-02-16 19:41:41 +0330

    fix baseURL in axiosInstance

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://hardwar-sharif.ir:8080/api",
+  baseURL: "https://api.hardwar-sharif.ir/api",
   headers: {
     "Content-Type": "application/json",
   },
